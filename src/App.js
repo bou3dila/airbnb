cd9e91cb3d1c2746d2380cb618afd6741618c6f3
bou3dila <bou3dilafiras@gmail.com>
2021-07-02 12:31:04 +0100

    start fetching the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {client} from './client';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(()=>{
+    client.getEntries()
+    .then((response)=>{
+      console.log(response)
+    })
+    .catch(console.error)
+  })
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className='container'>
+        <header>
+          <div className="wrapper">
+            <span>React and contentful</span>
+          </div>
+        </header>
+        <main>
+          <div className="wrapper"></div>
+        </main>
+      </div>
+      
     </div>
   );
 }
