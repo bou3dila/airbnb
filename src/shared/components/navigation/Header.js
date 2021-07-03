90fb80576ce0de82a40befb3fce8de35ff57ba02
bou3dila <bou3dilafiras@gmail.com>
2021-07-03 19:20:12 +0100

    dropdown list

--- src/shared/components/navigation/Header.js
+++ src/shared/components/navigation/Header.js
@@ -1,47 +1,67 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import desktop_logo from "./logo.png";
 import mobile_logo from "./mobile_logo.png";
-import SearchIcon from "@material-ui/icons/Search"
-import DehazeIcon from '@material-ui/icons/Dehaze';
-import LanguageIcon from "@material-ui/icons/Language"
-import { Avatar } from "@material-ui/core"
+import SearchIcon from "@material-ui/icons/Search";
+import DehazeIcon from "@material-ui/icons/Dehaze";
+import LanguageIcon from "@material-ui/icons/Language";
+import Dropdown from "./Dropdown";
+import { Avatar } from "@material-ui/core";
 
 import "./Header.css";
 
 export default function Header(props) {
+  const [logo, setLogo] = useState(desktop_logo);
+  const [dropdown, setDropdown] = useState(false);
 
-    const [logo, setLogo] = useState(desktop_logo);
+  useEffect(() => {
+    resize();
+  }, []);
 
-    useEffect(()=>{
-        resize();
-    },[])
-    const resize = () =>{
-        if(window.innerWidth > 960){
-            setLogo(desktop_logo)
-        }else {
-            setLogo(mobile_logo)
-        }
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(!dropdown);
     }
+  };
 
-    window.addEventListener('resize', resize)
+  const handleClickOutside = (e) => {
+      if( e.target.className !== "dropdown-link")
+            setDropdown(false);
+  };
+
+  const resize = () => {
+    if (window.innerWidth > 960) {
+      setLogo(desktop_logo);
+    } else {
+      setLogo(mobile_logo);
+    }
+  };
+
+  window.addEventListener("resize", resize);
+  document.addEventListener("mousedown", handleClickOutside);
   return (
     <div className="header">
-      <img src={logo} className="header__icon" alt="logo" />
-      <div className='header__center'>
+      <Router>
+        <img src={logo} className="header__icon" alt="logo" />
+        <div className="header__center">
           <input type="text" />
           <SearchIcon />
-      </div>
+        </div>
 
-      <div className='header__right'>
+        <div className="header__right">
           <p>Become a host </p>
-          
+
           <LanguageIcon />
-          <div className='header__avatar'>
-          <DehazeIcon  />
-          <Avatar />
+          <div className="header__avatar" onClick={onMouseEnter}>
+            <DehazeIcon />
+            <Avatar />
           </div>
-      </div>
+          {dropdown && <Dropdown />}
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/shared/components/navigation/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Sign up',
+        path: '/signup',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Log in',
+        path: '/login',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Host your home',
+        path: '/hosthome',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Host an experience',
+        path: '/hostexp',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Help',
+        path: '/help',
+        cName: 'dropdown-link'
+    },
+]
