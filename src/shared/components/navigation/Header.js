c8756d1084b9fa11149155139c9a389b4a0700b5
bou3dila <bou3dilafiras@gmail.com>
2021-07-04 14:35:11 +0100

    sidebar

--- src/shared/components/navigation/Header.js
+++ src/shared/components/navigation/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import desktop_logo from "./logo.png";
 import mobile_logo from "./mobile_logo.png";
@@ -8,16 +8,23 @@ import DehazeIcon from "@material-ui/icons/Dehaze";
 import LanguageIcon from "@material-ui/icons/Language";
 import Dropdown from "./Dropdown";
 import { Avatar } from "@material-ui/core";
+import ClearIcon from "@material-ui/icons/Clear";
+import { MenuItems } from "./MenuItems";
 
 import "./Header.css";
 
 export default function Header(props) {
   const [logo, setLogo] = useState(desktop_logo);
   const [dropdown, setDropdown] = useState(false);
+  const [click, setClick] = useState(false);
 
   useEffect(() => {
     resize();
   }, []);
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
 
   const onMouseEnter = () => {
     if (window.innerWidth < 960) {
@@ -28,11 +35,15 @@ export default function Header(props) {
   };
 
   const handleClickOutside = (e) => {
-      if( e.target.className !== "dropdown-link")
-            setDropdown(false);
+    if (e.target.className !== "dropdown-link") setDropdown(false);
+  };
+
+  const handleClick = () => {
+    setClick(!click);
   };
 
   const resize = () => {
+    setDropdown(false);
     if (window.innerWidth > 960) {
       setLogo(desktop_logo);
     } else {
@@ -55,11 +66,29 @@ export default function Header(props) {
           <p>Become a host </p>
 
           <LanguageIcon />
-          <div className="header__avatar" onClick={onMouseEnter}>
-            <DehazeIcon />
+          <div className="header__avatar " onClick={onMouseEnter}>
+            <DehazeIcon className="avatar" />
             <Avatar />
           </div>
           {dropdown && <Dropdown />}
+          <div className="menu_icon" onClick={handleClick}>
+            {click ? <ClearIcon /> : <DehazeIcon />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link
+                    className={click ? "nav-links active" : "nav-links"}
+                    to={item.path}
+                    onClick={() => setClick(false)}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </Router>
     </div>
